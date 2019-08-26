7abcda1607fc595f050bc6f94d722f06a1b75ab1
Yunsu Guk <krgoodnews@gmail.com>
2019-08-26 09:09:59 +0900

    프로젝트 생성 & CocoaPods 설치

---
+++ GodokChatting/GodokChatting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GodokChatting
+//
+//  Created by 국윤수 on 26/08/2019.
+//  Copyright © 2019 국윤수. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
