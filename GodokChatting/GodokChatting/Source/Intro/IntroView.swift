ee7cd3a36fb8141ec207ad97917acb2f8582b654
linsaeng <duwjdtmd91@gamil.com>
2019-08-31 17:40:41 +0900

    불필요한 코드 제거

--- GodokChatting/GodokChatting/Source/Intro/IntroView.swift
+++ GodokChatting/GodokChatting/Source/Intro/IntroView.swift
@@ -12,23 +12,26 @@ import SnapKit
 
 class IntroView: BaseView {
 
-    public let introTitle: UILabel = UILabel(frame: .zero).then {
-        $0.text = "Intro"
-        $0.textColor = .black
-    }
+    let gradientLayer = CAGradientLayer()
 
     override func setup() {
         super.setup()
 
-        addSubviews(introTitle)
+        gradientLayer.frame = self.bounds
+
+        let color1 = UIColor.yellow.cgColor
+        let color2 = UIColor(red: 1.0, green: 0, blue: 0, alpha: 1.0).cgColor
+        let color3 = UIColor.clear.cgColor as CGColor
+        let color4 = UIColor(white: 0.0, alpha: 0.7).cgColor
+        gradientLayer.colors = [color1, color2, color3, color4]
+
+        gradientLayer.locations = [0.0, 0.25, 0.75, 1.0]
+
+        self.layer.addSublayer(gradientLayer)
     }
 
     override func setupUI() {
         super.setupUI()
-
-        introTitle.snp.makeConstraints {
-            $0.center.equalToSuperview()
-        }
     }
 
     override func bind() {
